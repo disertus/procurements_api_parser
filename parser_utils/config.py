b82dd6cd07ad157148a75808455a49bac6e98976
disertus <61247961+disertus@users.noreply.github.com>
2021-10-19 09:26:50 +0300

    Update config.py

--- parser_utils/config.py
+++ parser_utils/config.py
@@ -1,5 +1,5 @@
 
 
-db_address = "/home/user/Documents/procurements_parser/prozorro_api_parser/parser_utils/tenders.db"
+db_address = "/prozorro_api_parser/parser_utils/tenders.db"
 sleep_time_if_disconnected = 5
 
